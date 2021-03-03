ce930588a619f0bf011c99340cb739592ac5b43f
Ismael Navarro <ismanapa@gmail.com>
2021-03-03 22:25:17 +0100

    Example 2 start

--- MyExampleApp/src/Home.tsx
+++ MyExampleApp/src/Home.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, Image, Text, Button } from 'react-native';
 
 
 export const Home: React.FC = () => {
   return (
     <SafeAreaView >
+      <Image
+        source={require('../assets/piggy.png')} />
+
+      <Text>Plain Poker</Text>
+
+      <Text>An app to estimate your awesome projects</Text>
+
+      <Button
+        onPress={() => { }}
+        title="Estimate" />
     </SafeAreaView>
   )
 };
