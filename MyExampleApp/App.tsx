5a310d06a7fc4dff5d4b64f288c3dfa42305b91b
Ismael Navarro <ismanapa@gmail.com>
2021-03-03 22:22:13 +0100

    Deleted base exercirse

--- MyExampleApp/App.tsx
+++ MyExampleApp/App.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import { Home } from './src/Home';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { ValuesList } from './src/ValuesList';
-
-const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator headerMode={'none'}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Values" component={ValuesList} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Home />
   );
 };
 
--- MyExampleApp/src/Home.tsx
+++ MyExampleApp/src/Home.tsx
@@ -1,58 +1,10 @@
-import { useNavigation } from '@react-navigation/native';
-import React, { useEffect } from 'react';
-import { Button, Image, SafeAreaView, StyleSheet, Text } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native';
 
-
-const HomeStyles = StyleSheet.create({
-  background: {
-    backgroundColor: '#041c2c',
-    height: '100%',
-    padding: 16,
-    justifyContent: 'space-around',
-    alignItems: 'center'
-  },
-  logo: {
-    borderRadius: 200,
-  },
-  title: {
-    color: 'white',
-    fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center'
-  },
-  subTitle: {
-    flex: 1,
-    color: 'white',
-    fontSize: 25,
-    textAlign: 'center'
-  }
-});
 
 export const Home: React.FC = () => {
-  const navigation = useNavigation();
-
-  useEffect(() => {
-    console.log('mounting');
-    return () => {
-      console.log('unmounting');
-    };
-  }, [])
-
   return (
-    <SafeAreaView style={HomeStyles.background}>
-
-      <Image
-        style={HomeStyles.logo}
-        source={require('../assets/piggy.png')} />
-
-      <Text style={HomeStyles.title}>Plain Poker</Text>
-
-      <Text style={HomeStyles.subTitle}>An app to estimate your awesome projects</Text>
-
-      <Button
-        color={'#00b5f1'}
-        onPress={() => { navigation.navigate('Values'); }}
-        title="Estimate" />
+    <SafeAreaView >
     </SafeAreaView>
   )
 };
